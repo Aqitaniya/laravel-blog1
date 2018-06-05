85575b205927b2c45f228650c5768205b56532ae
Aqitaniya <Your new email>
2018-06-05 19:12:16 +0300

    controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-use App\Task;
+//use App\Task;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,26 +30,31 @@ Route::get('/', function () {
     return view('welcome', compact('name','tasks'));
 });
 
-Route::get('/tasks', function (){
-    $tasks = Task::all();
-    //$tasks = App\Task::all();
-//    $tasks = DB::table('tasks')->latest()->get();
-    return view('tasks.index', compact('tasks'));
-});
+//Route::get('/tasks', function (){
+//    $tasks = Task::all();
+//    //$tasks = App\Task::all();
+////    $tasks = DB::table('tasks')->latest()->get();
+//    return view('tasks.index', compact('tasks'));
+//});
 
-Route::get('/tasks/{task}', function ($id){
-    $name = 'Dev';
-    $tasks = DB::table('tasks')->find($id);
-    dd($tasks);
-//    Task::incomplate()->get();
-    return view('welcome', compact('name','tasks'));
-});
+//Route::get('/tasks/{task}', function ($id){
+//    $name = 'Dev';
+//    $tasks = DB::table('tasks')->find($id);
+//    dd($tasks);
+////    Task::incomplate()->get();
+//    return view('welcome', compact('name','tasks'));
+//});
+//
+//Route::get('/tasks1/{task}', function ($id){
+//    $name = 'Dev';
+//    $task = Task::find($id); //For this form need create model
+//    //$task = App\Task::find($id); //For this form need create model
+//    //$task = DB::table('tasks')->find($id);
+//
+//    return view('tasks.show', compact('name','task'));
+//});
 
-Route::get('/tasks1/{task}', function ($id){
-    $name = 'Dev';
-    $task = Task::find($id); //For this form need create model
-    //$task = App\Task::find($id); //For this form need create model
-    //$task = DB::table('tasks')->find($id);
 
-    return view('tasks.show', compact('name','task'));
-});
+
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
